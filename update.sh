efb5e0639979ffc5edb08ca7c39093d20d81b3fc
patschwork <inetreg@pat-schmitz.de>
2023-05-05 09:41:29 +0200

    Update update.sh
    Fallback if jq is not found

--- update.sh
+++ update.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
 # sudo apt-get install jq
+# brew install jq
 # sudo apt-get install curl
 
 whichjq=$(which jq)
 whichcurl=$(which curl)
+whichpython3=$(which python3)
+whichpip=$(which pip)
 
 if [ -z "$whichjq" ]
 then
-      echo "jq is not installed. Cannot proceed."
-      exit
+      if [ -z "$whichpip" ]
+      then
+            echo "jq AND pip is not installed. One of tools are needed. Sorry, cannot proceed."
+            exit
+      fi
+      echo "jq not found, now installing 'lastversion' as fallback"
+      
+      pip install lastversion
+      if [ $? -ne 0 ]
+      then
+            echo "Because jq isn't installed, it was tried to install 'pip install lastversion' which also did not work. Sorry, cannot proceed."
+            echo "(You may try to install jq or pip manual (e.g. sudo apt install jq or brew install jq))"
+            exit
+      fi
 fi
 
 if [ -z "$whichcurl" ]
@@ -20,7 +35,12 @@ fi
 
 destdir=$PWD
 url_repo="patschwork/meta_grid_install_update"
-latest=$(curl --silent "https://api.github.com/repos/$url_repo/releases/latest" | jq -r .tag_name)
+if [ -z "$whichpip" ]
+then
+      latest=$(curl --silent "https://api.github.com/repos/$url_repo/releases/latest" | jq -r .tag_name)
+else
+      latest=$(lastversion $url_repo)
+fi
 file="meta_grid_updater.zip"
 
 
@@ -44,7 +64,7 @@ unzip $file -d meta-grid_install_or_update
 
 ls $destdir/install_settings.ini >/dev/null 2>&1
 if [ $? = 0 ]; then
-    echo "Backup existint install_settings.ini..."
+    echo "Backup existing install_settings.ini to install_settings.ini_BACKUP..."
     cp $destdir/install_settings.ini $destdir/install_settings.ini_BACKUP
     cp $destdir/install_settings.ini ./install_settings.ini_BACKUP
 fi
